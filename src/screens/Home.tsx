17976bada29ee4b63d3bc7cda85f32ae62b4c4e5
maaelattar <41055637+maaelattar@users.noreply.github.com>
2021-07-21 20:25:00 +0200

    Add loading more feature

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useInfiniteQuery } from "react-query";
 import { LoadingSkeleton, Repositories } from "../components";
+import { useIntersectionObserver } from "../hooks";
 import { fetchRepositories } from "../services";
 
 export const Home = () => {
@@ -9,11 +10,16 @@ export const Home = () => {
     data: repositoriesData,
     isError,
     error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
   } = useInfiniteQuery("repositories", fetchRepositories, {
     refetchOnWindowFocus: false,
 
     getNextPageParam: (_, pages) => pages.length + 1,
   });
+
+  const [loader] = useIntersectionObserver(fetchNextPage);
 
   if (isError && error instanceof Error) return <p>{error.message}</p>;
 
@@ -24,5 +30,17 @@ export const Home = () => {
       repositoriesData && <Repositories repositoriesData={repositoriesData} />
     );
 
-  return <div className="py-24">{renderResult()}</div>;
+  const renderLoadingMore = () =>
+    hasNextPage && (
+      <div className="loading-more" ref={loader}>
+        {isFetchingNextPage && <h2>Loading More...</h2>}
+      </div>
+    );
+
+  return (
+    <div className="py-24">
+      {renderResult()}
+      {renderLoadingMore()}
+    </div>
+  );
 };
