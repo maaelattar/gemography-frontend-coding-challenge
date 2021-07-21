5b4d8ec365c9762f01563b6473e2c0457eec95f7
maaelattar <41055637+maaelattar@users.noreply.github.com>
2021-07-21 18:00:01 +0200

    Update tailwind purge and container configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {},
   },
   variants: {
